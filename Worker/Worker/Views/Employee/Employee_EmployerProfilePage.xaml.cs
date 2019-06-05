9b870144ec5018e5f644ef7e5fb98596faf5a58e
OlesKorzhynskiy <oles.korzhynskiy@gmail.com>
2019-06-05 19:59:40 +0300

    Added employer profile page for employees

---
+++ Worker/Worker/Views/Employee/Employee_EmployerProfilePage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Worker.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Worker.Views.Employee
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Employee_EmployerProfilePage : ContentPage
+	{
+		public Employee_EmployerProfilePage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void OnReviewsPage(object sender, EventArgs e)
+        {
+            var user = (EmployerViewModel)BindingContext;
+            var reviewsPage = new ReviewsPage() { BindingContext = user.ReceivedReviews };
+            Navigation.PushAsync(reviewsPage);
+        }
+    }
+}
