4f1923d438584d30b02f9bfce75280bcabeca3ed
OlesKorzhynskiy <oles.korzhynskiy@gmail.com>
2019-06-15 23:03:26 +0300

    Calculate height for reviews page

--- Worker/Worker/Views/ReviewsPage.xaml.cs
+++ Worker/Worker/Views/ReviewsPage.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Worker.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,5 +17,22 @@ namespace Worker.Views
 		{
 			InitializeComponent ();
 		}
+
+        public int ReviewsListViewHeight
+        {
+            get
+            {
+                var elements = (ObservableCollection<ReviewViewModel>)BindingContext;
+                return elements?.Count * 170 ?? 0;
+            }
+        }
+
+        private void ListView_OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                ((ListView)sender).SelectedItem = null;
+            }
+        }
     }
 }
